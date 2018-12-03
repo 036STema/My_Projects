6c125360f7d949e7fb41b6cc0bd841a4c59016fb
036STema <44997174+036STema@users.noreply.github.com>
2018-12-03 12:43:13 +0300

    Update data_format.py

--- data_format.py
+++ data_format.py
@@ -1,6 +1,8 @@
 import json
 from pprint import pprint
-with  open("/home/rod/Документы/Python-developer/Python/data_format/newsafr.json") as datafile:
+from collections import Counter
+
+with  open('/home/rod/Документы/Python-developer/Python/data_format/newsafr.json') as datafile:
     json_data = json.load(datafile)
 
 current_events = []
@@ -8,14 +10,34 @@ list_words = []
 
 for key in json_data:
     list_istoch = (json_data[key]['channel']['items'])
-    #print(list_istoch)
 
 for key in list_istoch:
     current_events.append(key['description'])
-    #print(current_events)
-a = current_events[0].split()
-for x in a:
-    if len(x) > 6:
-        list_words.append(x)
 
-print(list_words)
+words = []
+
+for index in current_events:
+    new_index = index.split()
+    for word in new_index:
+        if len(word) > 6:
+            words.append(word)
+
+dict_words = Counter(words)
+list_values = []
+for value in dict_words.values():
+    list_values.append(value)
+
+list_values = sorted(list_values)
+list_values.reverse()
+top_10 = list_values[:10]
+#print(top_10)
+
+t_10 = []
+for x in dict_words.keys():
+    for y in top_10:
+        if y == dict_words[x]:
+            t_10.append(x)
+
+print(set(t_10))
+for word in set(t_10):
+    print(word, dict_words[x])
