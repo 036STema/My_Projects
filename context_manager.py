a323dbedd7d4dc8515c8eeedbf48ea094ae12e5f
036STema <44997174+036STema@users.noreply.github.com>
2018-12-04 11:48:36 +0300

    Update context_manager.py

--- context_manager.py
+++ context_manager.py
@@ -1,1 +1,14 @@
+import datetime
+import time
+with open('/home/rod/Документы/Python-developer/Python/data_format/newsafr.txt', 'r') as f:
+    start_time = time.time()
+    #print(time.time(), f.readline(), datetime.datetime.now() )
+    #print(start_time, "- %s seconds -" % (time.time() - start_time))
+
+with open('/home/rod/Документы/Python-developer/Python/data_format/newsafr.txt', 'r') as f:
+    start_time = time.time()
+    print(f.readline())
+    a = input('введите что-то')
+
+    print('Время запуска кода - {}\nВремя окончания работы кода - {}'.format(time.localtime(start_time), time.localtime(start_time)), "\nВремя работы - %s seconds " % (time.time() - start_time))
 
