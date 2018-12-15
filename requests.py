3b353f7e7f48171f2b5b6c689f84d63b9268087b
036STema <44997174+036STema@users.noreply.github.com>
2018-12-15 23:18:46 +0300

    Homework

--- requests.py
+++ requests.py
@@ -1,29 +1,37 @@
 import requests
 
-def translate_it(text):
-    """
-    YANDEX translation plugin
-    docs: https://tech.yandex.ru/translate/doc/dg/reference/translate-docpage/
-    https://translate.yandex.net/api/v1.5/tr.json/translate ?
-    key=<API-ключ>
-     & text=<переводимый текст>
-     & lang=<направление перевода>
-     & [format=<формат текста>]
-     & [options=<опции перевода>]
-     & [callback=<имя callback-функции>]
-    :param text: <str> text for translation.
-    :return: <str> translated text.
-    """
-    url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-    key = 'trnsl.1.1.20161025T233221Z.47834a66fd7895d0.a95fd4bfde5c1794fa433453956bd261eae80152'
+URL = 'https://translate.yandex.net/api/v1/tr.json/translate'
+params = 'id=32096114.5c0c2f12.baa47f5f-0-0&srv=tr-text&lang=%s-ru&reason=paste'
 
-    params = {
-        'key': key,
-        'lang': 'ru-en',
-        'text': text,
-    }
-    response = requests.get(url, params=params).json()
-    return ' '.join(response.get('text', []))
+def translate(text_for_translate, lang):
+    response = requests.post(URL, data={'text': text_for_translate}, params=params % lang)
+    body = response.json()
+    return body['text']
 
-a = translate_it('Привет')
-print(a)
+
+def translate_file(enter_path, lang, write_file):
+    with  open(enter_path, 'r') as file_txt:
+        for line in file_txt:
+            text_translate = translate(line, lang)
+            with  open(write_file, 'a') as new_file:
+                new_file.write(text_translate[0])
+
+def write_files(file):
+    with  open(file, 'w') as new_file:
+        new_file.write(body['text'])
+
+if __name__ == '__main__':
+    enter_an_action = input('Ввод текста = "i" , Перевод файла = "r" ')
+    enter_an_action = enter_an_action.lower()
+    if enter_an_action == 'i':
+        lang = input('С какого языка перевести  ')
+        text = input('Введите текст для перевода ')
+        text_after_translate = translate(text, lang)
+        print(text_after_translate)
+    elif enter_an_action == 'r':
+        enter_path = input('Укажите путь к файлу для перевода')
+        lang = input('С какого языка перевести  ')
+        write_file = input('Куда записать файл')
+        text_after_translate = translate_file(enter_path, lang, write_file)
+      
+
