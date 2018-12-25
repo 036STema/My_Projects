bb51f9251a01fdb161dcdd687175f7cc773a3bd0
036STema <44997174+036STema@users.noreply.github.com>
2018-12-25 13:11:50 +0300

    Update diploma.py

--- diploma.py
+++ diploma.py
@@ -1,7 +1,7 @@
 from urllib.parse import urlencode
 import requests
 import progressbar
-import time
+from time import sleep
 import json
 
 token = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
@@ -35,6 +35,7 @@ def main(user, group):
     'extended': 0,
     'fields': 'contacts'
     }
+    sleep(0.3)
     response = requests.get('https://api.vk.com/method/groups.isMember', params)
     #time.sleep(0.1)
     return response.json()['response']
