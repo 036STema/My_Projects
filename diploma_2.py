e7a0b83425163c428d1b494ed262622e68e48080
036STema <44997174+036STema@users.noreply.github.com>
2019-01-14 10:05:11 +0300

    Update diploma_2.py

--- diploma_2.py
+++ diploma_2.py
@@ -4,7 +4,7 @@ import json
 import sys
 
 TOKEN = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
-ID ='171691064'
+ID = '171691064'
 VALUE = '' #eshmargunov
 
 
