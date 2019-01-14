8b3ba7e1e0c8ef287edde751aa6e0179fe1d846b
036STema <44997174+036STema@users.noreply.github.com>
2019-01-14 09:57:18 +0300

    Update diploma_2.py

--- diploma_2.py
+++ diploma_2.py
@@ -4,7 +4,7 @@ import json
 import sys
 
 TOKEN = 'ed1271af9e8883f7a7c2cefbfddfcbc61563029666c487b2f71a5227cce0d1b533c4af4c5b888633c06ae'
-ID = '171691064'
+ID ='171691064'
 VALUE = '' #eshmargunov
 
 
@@ -42,30 +42,17 @@ def check_f(user, group):
     'extended': 0,
     'fields': 'contacts'
     }
-    number_of_attempts = 10
-    for i in range(number_of_attempts):
-        try:
-            response = requests.get('https://api.vk.com/method/groups.isMember', params)
-            response_json = response.json()
-            print("От АПИ успешно получен ответ")
-            return response_json['response']
-        except KeyError:
-            print("Не удалось получить ответ от АПИ, всего попыток: %s." % i)
-            if 'error' in response_json.keys():
-                print(response_json['error']['error_msg'])
-            sleep(1)
-    print("Не удалось получить ответ от АПИ, программа завершается.")
-    sys.exit()
+    response = get_response_api('https://api.vk.com/method/groups.isMember', params)
+    return response
 
 
 def search_friends(users, group):
     list_f = []
     if len(list_f) < 1:
         for user in users:
-            a = check_f(user, group)
-            #print(a)
-            if a == 1:
-                list_f.append(user)         
+            response = check_f(user, group)
+            if response == 1:
+                list_f.append(user)
     if len(list_f) >= 1:
         print('В {} группе есть ваши друзья'.format(group))
     else:
@@ -79,7 +66,7 @@ def main():
         'value': VALUE,
         'user_id': ID,
         'v':'5.92'
-        }  
+        }
     list_groups = get_response_api('https://api.vk.com/method/groups.get', params)['items']
     list_friends = get_response_api('https://api.vk.com/method/friends.get', params)['items']
     #b=list_friends[14:25]
